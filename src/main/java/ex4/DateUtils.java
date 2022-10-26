c8111553c606c5a46da452533f9b5bd6bdf35b65
Manuel <tyranidas@hotmail.com>
2022-10-26 14:37:05 +0200

    exo 4

--- src/main/java/ex4/DateUtils.java
+++ src/main/java/ex4/DateUtils.java
@@ -3,17 +3,25 @@ package ex4;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ * @author Manuel
+ *
+ */
 public class DateUtils {
 
-	public static String format(String pattern, Date date) {
-		
+
+	/**
+	 * Methode pour paramétrer une date avec une valeur par défaut
+	 * @param date
+	 * @param pattern
+	 * @return
+	 */
+	public static String format (Date date, String pattern) {
+		if (pattern.equals(null))
+		{
+			pattern = "dd/MM/yyyy HH:mm:ss";
+		}
 		SimpleDateFormat formateur = new SimpleDateFormat(pattern);
-		return formateur.format(date);
-	}
-	
-	public static String formatDefaut(Date date) {
-		
-		SimpleDateFormat formateur = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 		return formateur.format(date);
 	}
 }
